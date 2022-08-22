c2f041ed5d7c75425be6630ca8a558463ac9647c
Lucas Lucena <lucas.lucenak@gmail.com>
2022-08-21 22:15:22 -0300

    Add project

---
+++ src/main/java/com/lucaslucena/teclineossystemmail/TeclineOsSystemMailApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslucena.teclineossystemmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeclineOsSystemMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeclineOsSystemMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslucena/teclineossystemmail/TeclineOsSystemMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslucena.teclineossystemmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeclineOsSystemMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
