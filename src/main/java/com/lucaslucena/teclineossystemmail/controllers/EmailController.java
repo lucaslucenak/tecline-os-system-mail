e158082bb4805f67c6cc2e51b7972518c79ec520
Lucas Lucena <lucas.lucenak@gmail.com>
2022-08-21 22:58:45 -0300

    Update application.properties: Update database conection

--- src/main/java/com/lucaslucena/teclineossystemmail/controllers/EmailController.java
+++ src/main/java/com/lucaslucena/teclineossystemmail/controllers/EmailController.java
@@ -1,7 +1,16 @@
 package com.lucaslucena.teclineossystemmail.controllers;
 
+import com.lucaslucena.teclineossystemmail.dtos.EmailDto;
+import com.lucaslucena.teclineossystemmail.models.EmailModel;
 import com.lucaslucena.teclineossystemmail.services.EmailService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class EmailController {
@@ -10,5 +19,13 @@ public class EmailController {
 
     public EmailController(EmailService emailService) {
         this.emailService = emailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/lucaslucena/teclineossystemmail/services/EmailService.java
+++ src/main/java/com/lucaslucena/teclineossystemmail/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.lucaslucena.teclineossystemmail.services;
 
+import com.lucaslucena.teclineossystemmail.models.EmailModel;
 import com.lucaslucena.teclineossystemmail.repositories.EmailRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,7 @@ public class EmailService {
         this.emailRepository = emailRepository;
     }
 
+    public void sendEmail(EmailModel emailModel) {
 
+    }
 }
